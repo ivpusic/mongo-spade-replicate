762a63690d36a5b1f5d147e20052647274629738
ivpusic <pusic007@gmail.com>
2014-01-31 03:56:04 +0100

    added another host into config.py

--- config.py
+++ config.py
@@ -5,5 +5,6 @@ HOST_NAME = socket.gethostname()
 
 connected = {
     # agent_local_ip source : [[agent_name_destination, spade_platform_ip]]
-    '10.24.20.61': [['lubuntu1_receiver', HOST_SPADE_IP]]
+    '10.24.20.61': [['lubuntu1_receiver', HOST_SPADE_IP]],
+    '10.24.24.209': [['dell', HOST_SPADE_IP]]
 }
